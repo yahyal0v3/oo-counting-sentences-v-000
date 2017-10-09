9171885808f9f487b60ad1acb8b727366ebdf1f4
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-09 20:18:38 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
+    counter = 0
     self.split.each do |word|
       counter += 1 if word.include?(".") || word.include?("?") || word.include?("!")
     end
